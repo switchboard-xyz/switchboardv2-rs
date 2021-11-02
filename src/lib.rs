2d0168d9b31cd1f9b312877a537f165ce69bdb1c
mgild <mgild@users.noreply.github.com>
2021-11-01 22:13:46 -0400

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,18 @@ use solana_program::account_info::AccountInfo;
 use solana_program::program_error::ProgramError;
 pub use structs::{AggregatorAccountData, AggregatorRound, SwitchboardDecimal};
 
-anchor_lang::declare_id!("3TSYZ3oXt9e42TdXFuYVaQyLP5ZX3fWtMhaQReHPau5f");
+anchor_lang::declare_id!("GFerzWEGnbUn4ZvjVqdKhasNSFU16BkbYB1eUqxHMtDA");
 
 /// Given a Switchboard data feed account, this method will parse the account state.
 ///
 /// Returns the most recent resolution round that is considered valid for the aggregator.
 pub fn get_aggregator_result(
     switchboard_feed: &AccountInfo,
-) -> Result<AggregatorRound, ProgramError> {
-    let aggregator = AggregatorAccountData::new(switchboard_feed)?;
-    aggregator.get_result()
+) -> Result<u64, ProgramError> {
+    msg!("{}", std::mem::size_of::<AggregatorAccountData>());
+    msg!("a");
+    // let aggregator = AggregatorAccountData::new(switchboard_feed)?;
+    // msg!("a");
+    // aggregator.get_result()
+    0
 }
