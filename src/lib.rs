386b294426bdc449f23ad6a9bca4287dd4416e2d
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-02-16 14:22:34 -0700

    VrfRequestRandomness

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use anchor_lang::prelude::*;
-
 pub mod aggregator;
 pub mod decimal;
 pub mod error;
@@ -8,45 +6,4 @@ pub mod vrf;
 pub use aggregator::AggregatorAccountData;
 pub use vrf::VrfAccountData;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-
-#[program]
-pub mod switchboard_v2 {
-    pub use super::*;
-
-    pub fn vrf_request_randomness(
-        ctx: Context<VrfRequestRandomness>,
-        params: VrfRequestRandomnessParams,
-    ) -> ProgramResult {
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-#[instruction(params: VrfRequestRandomnessParams)] // rpc parameters hint
-pub struct VrfRequestRandomness<'info> {
-    #[account(signer)]
-    pub authority: AccountInfo<'info>,
-    #[account(mut)]
-    pub vrf: AccountInfo<'info>,
-    #[account(mut)]
-    pub oracle_queue: AccountInfo<'info>,
-    pub queue_authority: AccountInfo<'info>,
-    pub data_buffer: AccountInfo<'info>,
-    #[account(mut)]
-    pub permission: AccountInfo<'info>,
-    #[account(mut)]
-    pub escrow: AccountInfo<'info>,
-    #[account(mut)]
-    pub payer_wallet: AccountInfo<'info>,
-    #[account(signer)]
-    pub payer_authority: AccountInfo<'info>,
-    pub recent_blockhashes: AccountInfo<'info>,
-    pub program_state: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
-}
-#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct VrfRequestRandomnessParams {
-    pub permission_bump: u8,
-    pub state_bump: u8,
-}
+pub use vrf::VrfRequestRandomness;
--- src/vrf.rs
+++ src/vrf.rs
@@ -3,7 +3,9 @@
 use super::error::SwitchboardError;
 use anchor_lang::prelude::*;
 use bytemuck::{Pod, Zeroable};
-use solana_program::instruction::{AccountMeta, Instruction};
+use solana_program::entrypoint::ProgramResult;
+use solana_program::instruction::Instruction;
+use solana_program::program::invoke;
 use solana_program::pubkey::Pubkey;
 use std::cell::Ref;
 
@@ -381,3 +383,155 @@ impl VrfAccountData {
 unsafe impl Pod for VrfAccountData {}
 unsafe impl Zeroable for VrfAccountData {}
 
+#[derive(Accounts)]
+#[instruction(params: VrfRequestRandomnessParams)] // rpc parameters hint
+pub struct VrfRequestRandomness<'info> {
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub vrf: AccountInfo<'info>,
+    #[account(mut)]
+    pub oracle_queue: AccountInfo<'info>,
+    pub queue_authority: AccountInfo<'info>,
+    pub data_buffer: AccountInfo<'info>,
+    #[account(mut)]
+    pub permission: AccountInfo<'info>,
+    #[account(mut)]
+    pub escrow: AccountInfo<'info>,
+    #[account(mut)]
+    pub payer_wallet: AccountInfo<'info>,
+    #[account(signer)]
+    pub payer_authority: AccountInfo<'info>,
+    pub recent_blockhashes: AccountInfo<'info>,
+    pub program_state: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct VrfRequestRandomnessParams {
+    pub permission_bump: u8,
+    pub state_bump: u8,
+}
+
+impl<'info> VrfRequestRandomness<'info> {
+    fn discriminator() -> [u8; 8] {
+        return [230, 121, 14, 164, 28, 222, 117, 118];
+    }
+
+    pub fn get_instruction(
+        &self,
+        program_id: Pubkey,
+        params: VrfRequestRandomnessParams,
+    ) -> Result<Instruction, ProgramError> {
+        let accounts = self.to_account_metas(None);
+        msg!("account_metas len {:?}", accounts.len());
+
+        let mut data: Vec<u8> = VrfRequestRandomness::discriminator().try_to_vec()?;
+        let mut param_vec: Vec<u8> = params.try_to_vec()?;
+        data.append(&mut param_vec);
+
+        msg!("ixData {:?}", data);
+
+        let instruction = Instruction::new_with_bytes(program_id, &data, accounts);
+        Ok(instruction)
+    }
+
+    pub fn request_randomness(
+        &self,
+        program: AccountInfo<'info>,
+        state_bump: u8,
+        permission_bump: u8,
+    ) -> ProgramResult {
+        let cpi_params = VrfRequestRandomnessParams {
+            state_bump: state_bump,
+            permission_bump: permission_bump,
+        };
+        let instruction = self.get_instruction(program.key.clone(), cpi_params)?;
+        let account_infos = self.to_account_infos();
+        msg!("account_infos len {:?}", account_infos.len());
+
+        msg!("invoking CPI");
+        invoke(&instruction, &account_infos[..])
+    }
+
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        return vec![
+            self.authority.clone(),
+            self.vrf.clone(),
+            self.oracle_queue.clone(),
+            self.queue_authority.clone(),
+            self.data_buffer.clone(),
+            self.permission.clone(),
+            self.escrow.clone(),
+            self.payer_wallet.clone(),
+            self.payer_authority.clone(),
+            self.recent_blockhashes.clone(),
+            self.program_state.clone(),
+            self.token_program.clone(),
+        ];
+    }
+
+    fn to_account_metas(&self, is_signer: Option<bool>) -> Vec<AccountMeta> {
+        return vec![
+            AccountMeta {
+                pubkey: self.authority.key.clone(),
+                is_signer: self.authority.is_signer,
+                is_writable: self.authority.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.vrf.key.clone(),
+                is_signer: self.vrf.is_signer,
+                is_writable: self.vrf.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.oracle_queue.key.clone(),
+                is_signer: self.oracle_queue.is_signer,
+                is_writable: self.oracle_queue.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.queue_authority.key.clone(),
+                is_signer: self.queue_authority.is_signer,
+                is_writable: self.queue_authority.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.data_buffer.key.clone(),
+                is_signer: self.data_buffer.is_signer,
+                is_writable: self.data_buffer.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.permission.key.clone(),
+                is_signer: self.permission.is_signer,
+                is_writable: self.permission.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.escrow.key.clone(),
+                is_signer: self.escrow.is_signer,
+                is_writable: self.escrow.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.payer_wallet.key.clone(),
+                is_signer: self.payer_wallet.is_signer,
+                is_writable: self.payer_wallet.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.payer_authority.key.clone(),
+                is_signer: self.payer_authority.is_signer,
+                is_writable: self.payer_authority.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.recent_blockhashes.key.clone(),
+                is_signer: self.recent_blockhashes.is_signer,
+                is_writable: self.recent_blockhashes.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.program_state.key.clone(),
+                is_signer: self.program_state.is_signer,
+                is_writable: self.program_state.is_writable,
+            },
+            AccountMeta {
+                pubkey: self.token_program.key.clone(),
+                is_signer: self.token_program.is_signer,
+                is_writable: self.token_program.is_writable,
+            },
+        ];
+    }
+}
